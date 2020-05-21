968242e71cca86a86f84a9f2ef40a865f33ea80b
Carlos Rafael Mora Beltran <carlos.mora1135@uppuebla.edu.mx>
2020-05-21 09:27:28 -0500

    Actividad 3

--- AreaPoligonio/src/main/java/AreaPoligono.java
+++ AreaPoligonio/src/main/java/AreaPoligono.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class AreaPoligono {
     public static void main(String args[]){
         Scanner leer=new Scanner(System.in);
-        double apotema,perimetro,area;
-        System.out.println("Introduzca el perimetro del poligono");
-       perimetro=leer.nextDouble();
+        double apotema,perimetro,area,longitud;
+        int lados;
+        System.out.println("Introduzca el numero de lados del poligono");
+       lados=leer.nextInt();
+       System.out.println("Introduzca la longitud que miden los lados del poligono");
+       longitud=leer.nextDouble();
+     perimetro=longitud*lados;
         System.out.println("Introduzca el apotema del perimetro: ");
         apotema=leer.nextDouble();
         area=perimetro*apotema/2;
