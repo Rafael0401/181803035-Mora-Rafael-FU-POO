39951aab5230c7d73adee64a60a7827a30a19670
Carlos Rafael Mora Beltran <carlos.mora1135@uppuebla.edu.mx>
2020-05-21 08:38:07 -0500

    Actividades

---
+++ Cuadrado/src/main/java/Cuadrado.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Cuadrado {
+    public static void main(String args[]){
+        Scanner leer=new Scanner(System.in);
+        double a,area;
+        System.out.println("Introduzca el valor del cuadrado");
+        a=leer.nextInt();
+        area=a*a;
+        System.out.println("El area del cuadrado es de: "+area);
+        
+        
+    }
+}
